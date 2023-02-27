2f1827fc0fc67f79136ab90aafdf51728e1d09b7
DidierEPG <didierernestopereiragarcia@gmail.com>
2023-02-27 18:06:59 -0500

    homework-CS-ready

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,29 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var aux = num.split("").reverse();
+   var decimal = 0
+   for(var o = 0;o<aux.length;o++){
+    decimal = decimal + aux[o] * Math.pow(2,o)
+   }
+   return decimal
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   var binario = [];
+var n = num;
+for(var i = 0;i<num;i++){
+   if(n < 1) return binario.join("");
+   if(n % 2 === 0){
+      binario.unshift(0);
+      n = n / 2;
+   }else{
+      binario.unshift(1);
+      n = Math.floor(n) / 2;
+   }
+}
+   return binario.join("");
+}
 
 module.exports = {
    BinarioADecimal,
